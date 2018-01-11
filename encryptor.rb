9891952d1f66a90e48e52d051e38ef8c113ce7c0
Evan Wheeler <anon0mys87@gmail.com>
2018-01-11 10:26:05 -0700

    Encryptor that can handle files.

--- encryptor.rb
+++ encryptor.rb
@@ -13,7 +13,6 @@ class Encryptor
       cryp_message << cipher_for_rotation[letter]
     end
     cryp_message = cryp_message.join
-    puts "#{cryp_message}"
     return cryp_message
   end
 
@@ -25,16 +24,40 @@ class Encryptor
       cryp_message << cipher_for_rotation[letter]
     end
     cryp_message = cryp_message.join
-    puts "#{cryp_message}"
     return cryp_message
+  end
+
+  def encrypt_file(filename, rotation)
+    file = File.open("#{filename}", "r") #Create the file handle to the input file
+    message = file.read #Read the text of the input file
+    enc_message = encrypt(message, rotation) #Encrypt the text
+    encrypted_file = File.open("encrypted_#{filename}", "w") #Create a name for the output file
+    encrypted_file.write(enc_message) #Write out the text
+    encrypted_file.close
+    file.close#Close the file
+  end
+
+  def decrypt_file(filename, rotation)
+    file = File.open("#{filename}", "r") #Create the file handle to the input file
+    new_name = "#{filename}".gsub(/encrypted_/, 'decrypted')
+    enc_message = file.read #Read the text of the input file
+    message = decrypt(enc_message, rotation) #Decrypt the text
+    decrypted_file = File.open("#{new_name}", "w") #Create a name for the output file
+    decrypted_file.write(message) #Write out the text
+    decrypted_file.close
+    file.close #Close the file
   end
 
 end
 
 e = Encryptor.new
+print "What is the file you want to encrypt? (Include file extension) > "
+encrypt_this = gets.chomp
 print "How much rotation do you want for your cipher? > "
 rotation = gets.chomp.to_i
-puts "What is the message you want to encrypt? > "
-input = gets.chomp.downcase
-input = e.encrypt(input, rotation)
-e.decrypt(input, rotation)
+e.encrypt_file(encrypt_this, rotation)
+print "What is the file you want to decrypt? (Include file extension) > "
+decrypt_this = gets.chomp
+print "How much rotation was used to encrypt the file? > "
+rotation = gets.chomp.to_i
+e.decrypt_file(decrypt_this, rotation)
